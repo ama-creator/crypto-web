124acf0ed19e2f4c0a32b273a2c5f323fd2a5669
Anatoly <markduho0@gmail.com>
2022-10-16 23:31:27 +0300

    first commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,41 @@
 import React from 'react';
 
+// import images 
+import Image from '../assets/img/hero-img.png'
+// import icons 
+import { IoIosArrowDroprightCircle } from 'react-icons/io'
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section>
+      <div className='container mx-auto'>
+        <div className='flex flex-col items-center lg:flex-row'>
+          <div className='flex-1'>
+
+            <div data-aos="fade-down" data-aos-delay="400" className='bg-white/10 p-1 mb-6 rounded-full pl-1 pr-3 max-w-[365px]'>
+              <div className='flex items-center justify-between text-sm lg:text-base'>
+                <div className='bg-white text-darkblue rounded-full font-medium py-1 px-4'>75% SAVE</div>
+                <div>For the Black Friday weekend</div>
+
+              </div>
+
+            </div>
+
+
+            <h1 data-aos="fade-down" data-aos-delay="500" className='text-[32px] lg:text-[64px] font-bold leading-light mb-6'>Fastest & secure platform to invest in crypto.</h1>
+            <p data-aos="fade-down" data-aos-delay="600" className='max-w-[440px] leading-relaxed mb-8'>Buy and sell cryptocurrencies, trusted by 10M wallets with over $30 billion in transactions.</p>
+            <button className='btn gap-x-6 pl-6 text-sm lg:h-16 lg:text-base' data-aos="fade-down" data-aos-delay="700">
+              Try for free
+              <IoIosArrowDroprightCircle className='text-2xl lg:text-3xl' />
+              </button>
+          </div>
+          <div className='flex'>
+            <img src={Image} alt="" data-aos="fade-up" data-aos-delay="600" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
